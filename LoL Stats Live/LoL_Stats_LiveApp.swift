875673df59673f22962a2b7c0dfe06677db5de2c
Arush <arush.yadlapati@gmail.com>
2025-05-18 14:19:41 -0700

    Initial Commit

---
+++ LoL Stats Live/LoL_Stats_LiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoL_Stats_LiveApp.swift
+//  LoL Stats Live
+//
+//  Created by Arush Yadlapati on 5/18/25.
+//
+
+import SwiftUI
+
+@main
+struct LoL_Stats_LiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
